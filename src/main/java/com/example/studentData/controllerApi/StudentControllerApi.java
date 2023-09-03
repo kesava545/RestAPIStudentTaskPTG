b7bf4de9920f5c880789d76640b393a7981f1de4
shyam <shyamukarimsetti@gmail.com>
2023-09-03 21:34:19 +0530

    Initial commit

---
+++ src/main/java/com/example/studentData/controllerApi/StudentControllerApi.java
@@ -0,0 +1,39 @@
+package com.example.studentData.controllerApi;
+
+import com.example.studentData.entity.StudentEntity;
+import com.example.studentData.repository.StudentRepository;
+import com.example.studentData.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class StudentControllerApi {
+    @Autowired
+    StudentService studentService;
+    @PostMapping("/createdata/insert")
+    public StudentEntity createstudentdata(@RequestBody StudentEntity studentEntity)
+    {
+        return studentService.createstudentdata(studentEntity);
+    }
+    @GetMapping("/getallstudentsdata/all")
+    public List< StudentEntity > getstudentsdatas()
+    {
+        return studentService.getstudentsdata();
+    }
+    @GetMapping("/getstudentdata/{stdId}")
+    public StudentEntity getstudentdatabyid(@PathVariable  Long stdId){
+        return studentService.getstudentdatabyid(stdId);
+    }
+@DeleteMapping("/deletestudentdatabyid/{stdId}")
+    public void deletestudentdatabyid(@PathVariable  Long stdId){
+       studentService.deletestudentdatabyid(stdId);
+
+    }
+    @PutMapping ("/updatestudentdatabyid/{stdId}")
+    public StudentEntity updatestudentdata (@PathVariable  Long stdId ,@RequestBody StudentEntity updatestudentdata){
+
+        return studentService.updatestudentdata(stdId,updatestudentdata);
+    }
+}
---
+++ src/main/java/com/example/studentData/service/StudentService.java
@@ -0,0 +1,43 @@
+package com.example.studentData.service;
+
+import com.example.studentData.entity.StudentEntity;
+import com.example.studentData.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
+    public StudentEntity createstudentdata(StudentEntity studentEntity)
+    {
+        return studentRepository.save(studentEntity);
+    }
+
+    public List< StudentEntity > getstudentsdata()
+    {
+        return studentRepository.findAll();
+    }
+    public StudentEntity getstudentdatabyid(Long stdId){
+        return  studentRepository.findById(stdId).orElse(null);
+    }
+    public void deletestudentdatabyid(Long stdId){
+        studentRepository.deleteById(stdId);
+
+    }
+    public StudentEntity updatestudentdata(Long stdId, StudentEntity updatestudentdata) {
+        StudentEntity studentEntity1 = getstudentdatabyid(stdId);
+        if (studentEntity1 != null) {
+            studentEntity1.setStdName(updatestudentdata.getStdName());
+            studentEntity1.setStdAddress(updatestudentdata.getStdAddress());
+            return studentRepository.save(studentEntity1);
+        }
+        return null;
+    }
+
+
+
+}
